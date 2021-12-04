790e528d6d8df15a075ada02d4627dd4fd6821e1
Krishna Agarwal <90515734+krishnaa15@users.noreply.github.com>
2021-12-04 19:20:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,6 @@ from juno.gameround import GameRound
 from juno.hand import Hand
 from juno.player import Player
 
-deck=Deck()
-cards=Card.create_standard_52_cards()
-deck.add_cards(cards)
-
-hand1= Hand()
-hand2= Hand()
-hand3= Hand()
-hand4= Hand()
-
 print("Welcome to JUNO!")
 
 name1= input("Player1, please enter your name: ")
@@ -22,25 +13,78 @@ name4= input("Player4, please enter your name: ")
 
 print("\n")
 
-player1=Player(name=name1, hand=hand1)
-player2=Player(name=name2, hand=hand2)
-player3=Player(name=name3, hand=hand3)
-player4=Player(name=name4, hand=hand4)
-players=[player1, player2, player3, player4]
-
-game_round=GameRound(deck=deck, players=players)
-game_round.play()
-
-for player in players:
-    print(f"{player.name} your cards are: {player.hand}.\n")
+c1=0
+c2=0
+c3=0 
+c4=0
     
-for player in players: 
-    index, hand_name, hand_cards = player.best_hand()
-    hand_cards_strings = [str(card) for card in hand_cards]
-    hand_cards_string = " and ".join(hand_cards_strings)
-    print(f"{player.name} has a {hand_name} with a {hand_cards_string}.")
+score1=int(c1)
+score2=int(c2)
+score3=int(c3)
+score4=int(c4)   
 
-winning_player = max(players)
+choice=1
+while choice != '0':
+    deck=Deck()
+    cards=Card.create_standard_52_cards()
+    deck.add_cards(cards)
+    
+    hand1= Hand()
+    hand2= Hand()
+    hand3= Hand()
+    hand4= Hand()
+    
+    player1=Player(name=name1, hand=hand1)
+    player2=Player(name=name2, hand=hand2)
+    player3=Player(name=name3, hand=hand3)
+    player4=Player(name=name4, hand=hand4)
+    players=[player1, player2, player3, player4]
+    
+    game_round=GameRound(deck=deck, players=players)
+    game_round.play() 
+    
+    for player in players:
+        print(f"{player.name} your cards are: {player.hand}.\n")
+        
+    for player in players: 
+        index, hand_name, hand_cards = player.best_hand()
+        hand_cards_strings = [str(card) for card in hand_cards]
+        hand_cards_string = " and ".join(hand_cards_strings)
+        print(f"{player.name} has a {hand_name} with a {hand_cards_string}.")
+    
+    winning_player = max(players)
+    if(winning_player==player1):
+        score1=score1+1
+    elif(winning_player==player2):
+        score2=score2+1
+    elif(winning_player==player3):
+        score3=score3+1
+    elif(winning_player==player4):
+        score4=score4+1
+        
+    print(f"\nThe winner is {winning_player.name}!")
+    print("The scores are: ")
+    print(f"{player1.name}: {score1}")
+    print(f"{player2.name}: {score2}")
+    print(f"{player3.name}: {score3}")
+    print(f"{player4.name}: {score4}")
+  
+    scores=[score1,score2,score3,score4]
+    choice=input("Press 1 if all players want to continue playing, else press 0 to quit: ")
 
-print(f"\nThe winner is {winning_player.name}!")
-print("\nThanks for playing")
+print("\nThe final scores are: ")
+print(f"{player1.name}: {score1}")
+print(f"{player2.name}: {score2}")
+print(f"{player3.name}: {score3}")
+print(f"{player4.name}: {score4}\n")
+
+if score1==max(scores):
+    print(f"{player1.name} is the winner with score {score1}")
+elif score2==max(scores):
+    print(f"{player2.name} is the winner with score {score2}")
+elif score3==max(scores):
+    print(f"{player3.name} is the winner with score {score3}")
+elif score4==max(scores):
+    print(f"{player4.name} is the winner with score {score4}")
+
+print("\nTHANKS FOR PLAYING!!")
